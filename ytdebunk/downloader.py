8517b71357b3094a238502825e6b39cd1cf31a6f
Sazzad Hissain Khan <hissain.khan@gmail.com>
2025-03-23 22:51:39 +0600

    Added CLI feature for  command

--- ytdebunk/downloader.py
+++ ytdebunk/downloader.py
@@ -1,15 +1,9 @@
 import os
-import argparse
 import yt_dlp
 
 def download_audio(video_url, output_dir="downloads", output_audio="audio.mp3"):
-    """
-    Downloads audio from a YouTube video and saves it as an MP3 file.
-    :param video_url: URL of the YouTube video
-    :param output_dir: Directory to save the downloaded file
-    :param output_audio: Name of the output audio file (MP3 format)
-    """
     os.makedirs(output_dir, exist_ok=True)
+    os.remove(os.path.join(output_dir, output_audio)) if os.path.exists(os.path.join(output_dir, output_audio)) else None
     
     ydl_opts = {
         'format': 'bestaudio/best',  # Select best audio format
@@ -21,6 +15,7 @@ def download_audio(video_url, output_dir="downloads", output_audio="audio.mp3"):
         'outtmpl': os.path.join(output_dir, os.path.splitext(output_audio)[0]),
         'noplaylist': True,  # Download a single video, not a playlist
         'progress_hooks': [lambda d: print(f"Downloading: {d.get('_percent_str', '0%')} complete")],
+        'cachedir': False,  # Disable caching, as we're not going to download the same video again
     }
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
